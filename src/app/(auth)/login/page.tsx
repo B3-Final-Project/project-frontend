417917471bbc4724c7a9db98279f2e16dcd7e680
Maximilian <mpinderwhite@gmail.com>
2025-03-11 17:56:12 +0100

    feat: added login component

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+import { LoginForm } from "@/components/login/LoginComponent";
+
+export default function page(){
+  return <div className={'flex w-full h-screen items-center justify-center'}><LoginForm/></div>
+}
