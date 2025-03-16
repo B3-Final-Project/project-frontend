f92782d8b08a25b3fef794e29bf6765482eb3441
Maximilian <mpinderwhite@gmail.com>
2025-03-16 19:45:23 +0100

    chore: styling

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,7 @@
 import { LoginForm } from "@/components/login/LoginComponent";
 
 export default function page(){
-  return <div className={'flex w-full h-screen items-center justify-center'}><LoginForm/></div>
+  return <div className={'flex w-full h-screen items-center justify-center'}>
+    <LoginForm/>
+  </div>
 }
