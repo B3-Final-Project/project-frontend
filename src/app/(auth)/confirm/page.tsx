53b955e5d4edeb5c71d7bceb1f759918d9c15fb9
Maximilian <mpinderwhite@gmail.com>
2025-03-23 17:32:25 +0100

    feat: added base ConfirmComponent.tsx

---
+++ src/app/(auth)/confirm/page.tsx
@@ -0,0 +1,7 @@
+import { ConfirmComponent } from "@/components/auth/confirm/ConfirmComponent";
+
+export default function page() {
+    return <div className={'flex w-full h-screen items-center justify-center'}>
+          <ConfirmComponent />
+    </div>
+}
