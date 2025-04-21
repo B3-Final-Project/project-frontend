b42c95dbf23521622bf25a0d3a309e86b214866f
Maximilian <mpinderwhite@gmail.com>
2025-04-21 12:01:09 +0200

    feat: update environment variables for Cognito and clean up HomePage component

--- src/app/(auth)/confirm/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ConfirmComponent } from "@/components/auth/confirm/ConfirmComponent";
-
-export default function page() {
-    return <div className={'flex w-full h-screen items-center justify-center'}>
-          <ConfirmComponent />
-    </div>
-}
