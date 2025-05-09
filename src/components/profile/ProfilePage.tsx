8689c0ac4134c2cc74a404631d712ea793906b37
Maximilian <mpinderwhite@gmail.com>
2025-05-09 12:26:33 +0200

    fix(profile): remove unused PACK button from ProfilePage component

--- src/components/profile/ProfilePage.tsx
+++ src/components/profile/ProfilePage.tsx
@@ -2,14 +2,11 @@
 import { ProfileDialogContent } from "@/components/profile/ProfileDialogContent";
 import { ProfileHeader } from "@/components/profile/ProfileHeader";
 import { ProfileAvatar } from "@/components/profile/ProfileAvatar";
-import { Button } from "@/components/ui/button";
-import { BoosterRouter } from "@/lib/routes/booster";
 
 export default function ProfilePage() {
 
   return (
     <div className="h-full w-full flex flex-col justify-between">
-      <Button onClick={() => BoosterRouter.getMatches()}>PACK</Button>
       <ProfileHeader />
       <div className={"w-full h-full bg-background shadow-lg flex flex-col items-center justify-center"}>
         <ProfileAvatar />
