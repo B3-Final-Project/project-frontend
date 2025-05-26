c9533149fbbfc6e6e8b21c6b994a992a3230c8ad
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2025-05-26 22:33:49 +0200

    feat(config): enhance next.config.ts with image remote patterns and webpack hot reload settings (#34)

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone"
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "holomatch-images.s3.eu-west-3.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      }
+    ]
+  },
+  webpack: (config) => {
+    // Configuration pour le hot reload dans Docker
+    config.watchOptions = {
+      poll: 1000, // Vérifie les changements toutes les secondes
+      aggregateTimeout: 300, // Délai avant de déclencher une reconstruction
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
