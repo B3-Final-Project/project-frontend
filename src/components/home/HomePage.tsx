d00ebb7d2b9350a6ac54b44b68bece4c8aa7ed59
Maximilian <mpinderwhite@gmail.com>
2025-06-04 14:45:48 +0200

    feat(homepage): added placeholder homepage

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,8 +1,35 @@
 'use client'
+import { useAuth } from "react-oidc-context";
+import Image from "next/image";
+import { Quantico } from "next/font/google";
+import { clsx } from "clsx";
+import { SignInButton } from "@/components/auth/SignInButton";
+
+const quantico = Quantico({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
+  const auth = useAuth()
+
+  if (auth.isLoading || !auth.user) {
+    return (
+      <main className={'flex flex-col items-center justify-center gap-10 h-screen'}>
+        <Image src={"/logo.png"} alt={"holomatch logo"} width={557} height={448}/>
+        <h1 className={clsx(quantico.className, 'text-4xl text-center')}>HOLOMATCH</h1>
+        <p>Ouvre ton pack, découvre des profils
+          et trouve ton match</p>
+        <SignInButton/>
+      </main>
   );
+  } else {
+    return (
+      <main className="flex flex-col items-center justify-center h-screen">
+        <Image src={"/logo.png"} alt={"holomatch logo"} width={278} height={224}/>
+        <h1 className={clsx(quantico.className, 'text-4xl text-center mt-10')}>Welcome, {auth.user.profile.email}!</h1>
+        <p className="text-lg mt-4">You are logged in.</p>
+      </main>
+    );
+  }
 }
