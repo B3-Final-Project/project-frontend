fc8145fe40f39a438a011cc7befd2206281b54fe
Keil-ENZO <enzo.keil06@icloud.com>
2025-07-04 09:00:35 +0200

    fix: correction review getBoosters

--- src/hooks/react-query/boosters/index.ts
+++ src/hooks/react-query/boosters/index.ts
@@ -12,7 +12,9 @@ export function useGetBooster(count: number, boosterType?: string | null) {
   const queryClient = useQueryClient();
   return useMutation({
     mutationKey: ["booster", boosterType],
-    mutationFn: () => BoosterRouter.getBoosters({ boosterType: boosterType ? Number(boosterType) : undefined }),
+    mutationFn: () => BoosterRouter.getBoosters({ 
+      boosterType: boosterType ? Number(boosterType) : undefined 
+    }),
     onSuccess: (data) => {
       queryClient.setQueryData(["booster", count, boosterType], data);
     },
--- src/lib/routes/booster/index.ts
+++ src/lib/routes/booster/index.ts
@@ -1,30 +1,18 @@
-import { authenticatedAxios } from "@/lib/auth-axios";
 import { BoosterPackDto } from "@/lib/routes/booster/dto/booster.dto";
 import { UserCardDto } from "@/lib/routes/booster/dto/user-card.dto";
 import { RESTServerRoute } from "@/lib/routes/server";
 import { createFetcher } from "@/lib/utils";
 
 interface GetBoostersParams {
-  type?: string;
   boosterType?: number;
 }
 
 export class BoosterRouter {
   public static getBooster = createFetcher(RESTServerRoute.REST_BOOSTER, "GET");
-  public static getBoosters = async (params?: GetBoostersParams): Promise<UserCardDto[]> => {
-    const parsedParams = params?.type ? {
-      boosterType: Number(params.type) 
-    } : undefined;
-    
-    const config = {
-      method: "GET",
-      url: RESTServerRoute.REST_TEN_BOOSTERS,
-      params: parsedParams
-    };
-    
-    const response = await authenticatedAxios<{ data: UserCardDto[] }>(config);
-    return response.data.data;
-  };
+  public static getBoosters = createFetcher<UserCardDto[], GetBoostersParams>(
+    RESTServerRoute.REST_TEN_BOOSTERS,
+    "GET",
+  );
   public static getAvailablePacks = createFetcher<BoosterPackDto[]>(
     RESTServerRoute.REST_AVAILABLE_PACKS,
     "GET",
