c7c0574e99c01e643d8ee6c0fa052fffd26e0716
Maximilian <mpinderwhite@gmail.com>
2025-07-07 10:35:21 +0200

    refactor: remove unused imports in stats-utils.ts

--- src/lib/utils/stats-utils.ts
+++ src/lib/utils/stats-utils.ts
@@ -1,6 +1,3 @@
-import { formatRelationshipTypeEnum } from "@/lib/utils/enum-utils";
-import { toNumber } from "es-toolkit/compat";
-
 export const CHART_COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884D8', '#82CA9D'];
 
 // DTO Interfaces matching backend structure
